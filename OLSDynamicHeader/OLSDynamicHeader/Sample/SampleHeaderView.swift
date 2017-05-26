d6043b089b1d61d3e1e0d19e87e2d7c91950011b
Omar Hagopian <clackmac@gmail.com>
2017-05-26 19:18:13 -0300

    Code improvements

--- OLSDynamicHeader/OLSDynamicHeader/Sample/SampleHeaderView.swift
+++ OLSDynamicHeader/OLSDynamicHeader/Sample/SampleHeaderView.swift
@@ -10,23 +10,23 @@ import UIKit
 
 class SampleHeaderView: OLSDynamicHeaderView {
 
-    let MIN_TEXT_SPACING: CGFloat = 0
-    let MAX_TEXT_SPACING: CGFloat = 0
+    let minHeaderHeight: CGFloat = 64 //navigation bar + status
+    let maxHeaderHeight: CGFloat = 260
 
-    let MIN_TITLE_FONT_SIZE: CGFloat = 18
-    let MAX_TITLE_FONT_SIZE: CGFloat = 35
+    let minLabelSpacing: CGFloat = 0
+    let maxLabelSpacing: CGFloat = 0
 
-    let MIN_HEIGHT: CGFloat = 64 //navigation bar + status
-    let MAX_HEIGHT: CGFloat = 260
+    let minTitleFontSize: CGFloat = 18
+    let maxTitleFontSize: CGFloat = 35
 
-    let MIN_BOTTOM_SPACING: CGFloat = 5
-    let MAX_BOTTOM_SPACING: CGFloat = 16
+    let minBottomSpacing: CGFloat = 5
+    let maxBottomSpacing: CGFloat = 16
 
-    let MIN_IMAGE_ALPHA: CGFloat = 0
-    let MAX_IMAGE_ALPHA: CGFloat = 1.6
+    let minImageAlpha: CGFloat = 0
+    let maxImageAlpha: CGFloat = 1.6
 
-    let MIN_BACKGROUND_ALPHA: CGFloat = 0.3
-    let MAX_BACKGROUND_ALPHA: CGFloat = 2.0
+    let minBackgroundAlpha: CGFloat = 0.3
+    let maxBackgroundAlpha: CGFloat = 2.0
 
     @IBOutlet weak var profileImageView: UIImageView!
     @IBOutlet weak var backgroundImageView: UIImageView!
@@ -42,7 +42,6 @@ class SampleHeaderView: OLSDynamicHeaderView {
 
         translatesAutoresizingMaskIntoConstraints = false
         profileImageView.layer.cornerRadius = 12
-
     }
 
     override class func viewInstance() -> SampleHeaderView {
@@ -52,11 +51,11 @@ class SampleHeaderView: OLSDynamicHeaderView {
     }
 
     override func maxHeight() -> CGFloat {
-        return MAX_HEIGHT
+        return maxHeaderHeight
     }
 
     override func minHeight() -> CGFloat {
-        return MIN_HEIGHT
+        return minHeaderHeight
     }
 
     override func resize(withProgress progress: CGFloat) {
@@ -66,25 +65,25 @@ class SampleHeaderView: OLSDynamicHeaderView {
         let headerDistance = maxHeight - minHeight
         let progressConstantValue = headerDistance - (headerDistance * progress)
 
-        let bottomValue = progressValue(MIN_BOTTOM_SPACING, MAX_BOTTOM_SPACING, progress: progress)
+        let bottomValue = progressValue(minBottomSpacing, maxBottomSpacing, progress: progress)
         bottomSpacingLayoutConstraint.constant = progressConstantValue + bottomValue
 
-        labelSpacingLayoutConstraint.constant = progressValue(MIN_TEXT_SPACING, MAX_TEXT_SPACING, progress: progress)
+        labelSpacingLayoutConstraint.constant = progressValue(minLabelSpacing, maxLabelSpacing, progress: progress)
 
-        let fontFinalValue = progressValue(MIN_TITLE_FONT_SIZE, MAX_TITLE_FONT_SIZE, progress: progress)
+        let fontFinalValue = progressValue(minTitleFontSize, maxTitleFontSize, progress: progress)
         titleLabel.font = UIFont(name: titleLabel.font!.familyName, size: fontFinalValue)
 
-        profileImageView.alpha = progressValue(MIN_IMAGE_ALPHA, MAX_IMAGE_ALPHA, progress: progress)
-        backgroundImageView.alpha = progressValue(MIN_BACKGROUND_ALPHA, MAX_BACKGROUND_ALPHA, progress: progress)
+        profileImageView.alpha = progressValue(minImageAlpha, maxImageAlpha, progress: progress)
+        backgroundImageView.alpha = progressValue(minBackgroundAlpha, maxBackgroundAlpha, progress: progress)
     }
 
     override func overflow(withPoints points: CGFloat) {
         //Reset everything
-        bottomSpacingLayoutConstraint.constant = MAX_BOTTOM_SPACING - points
-        labelSpacingLayoutConstraint.constant = MAX_TEXT_SPACING
-        titleLabel.font = UIFont(name: titleLabel.font!.familyName, size: MAX_TITLE_FONT_SIZE)
-        profileImageView.alpha = MAX_IMAGE_ALPHA;
-        backgroundImageView.alpha = MAX_BACKGROUND_ALPHA;
+        bottomSpacingLayoutConstraint.constant = maxBottomSpacing - points
+        labelSpacingLayoutConstraint.constant = maxLabelSpacing
+        titleLabel.font = UIFont(name: titleLabel.font!.familyName, size: maxTitleFontSize)
+        profileImageView.alpha = maxImageAlpha;
+        backgroundImageView.alpha = maxBackgroundAlpha;
 
         //Fun image transformation =D
         var headerTransform = CATransform3DIdentity
